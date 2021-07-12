2dcb84ced110bb7312d578da29e081602d7a204e
cristian358 <83902584+cristian358@users.noreply.github.com>
2021-07-12 16:27:49 +0300

    Show merchant; Header
    1. Fixing merchant controller for properly showing merchant information.
    2. Change Merchant Show Page.
    3. Adding style in Merchant Show Page
    4. Adding Header style
    5. Change some style from Merchant table

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -21,23 +21,23 @@ class ArticlesController < ApplicationController
 
   
   def show
-    @articles = Article.find(params[:id])
-    @comment = Comment.new(article_id: @articles.id)
-    @contactmethod = params[:contactmethod]
-    @ad_type = params[:ad_type]
-    respond_to do |format|
-      if @ad_type = "one"
-        # flash[:notice] = "Widget was successfully created."
-        format.json { render :show, status: :ok, location: @articles }
-      else
-        flash[:notice] = "Widget was successfully created."
-        format.json { render json: @articles.errors, status: :unprocessable_entity }
-      end
-    end
+     @article = Article.find(params[:id])
+    # # @comment = Comment.new(article_id: @articles.id)
+    # @contactmethod = params[:contactmethod]
+    # @ad_type = params[:ad_type]
+    # respond_to do |format|
+    #   if @ad_type = "one"
+    #     # flash[:notice] = "Widget was successfully created."
+    #     format.json { render :show, status: :ok, location: @articles }
+    #   else
+    #     flash[:notice] = "Widget was successfully created."
+    #     format.json { render json: @articles.errors, status: :unprocessable_entity }
+    #   end
+    # end
   end
-  def collection_radio_buttons(method, collection, value_method, text_method, options = {}, html_options = {}, &block)
-  @template.collection_radio_buttons(@object_name, method, collection, value_method, text_method, objectify_options(options), @default_html_options.merge(html_options), &block)
-end
+  # def collection_radio_buttons(method, collection, value_method, text_method, options = {}, html_options = {}, &block)
+  # @template.collection_radio_buttons(@object_name, method, collection, value_method, text_method, objectify_options(options), @default_html_options.merge(html_options), &block)
+# end
 
 
 
