bb00f68c16384ba069dc8287d92a97f2f27f798d
cristian358 <83902584+cristian358@users.noreply.github.com>
2021-07-13 21:35:59 +0300

    Editing merchants
    1. Create def update function.
    2. Create editing form.
    3. Fixing show form.
    4. Fixing header style issues.
    5. Fixing show form (user can press on name on the table and they will be redirected to show page).

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,6 +1,7 @@
 class ArticlesController < ApplicationController
   
   before_action :authenticate_user!
+  before_action :set_article, only: %i[ show edit update destroy ]
   def index
     @article = Article.new
     @articles = Article.text_search(params[:query], params[:ad_type]).page(params[:page]).per_page(40)
@@ -38,8 +39,30 @@ class ArticlesController < ApplicationController
   # def collection_radio_buttons(method, collection, value_method, text_method, options = {}, html_options = {}, &block)
   # @template.collection_radio_buttons(@object_name, method, collection, value_method, text_method, objectify_options(options), @default_html_options.merge(html_options), &block)
 # end
+def edit
+end
+  # PATCH/PUT /friends/1 or /friends/1.json
+def update
+  respond_to do |format|
+    if @article.update(article_params)
+      format.html { redirect_to @article, notice: "Friend was successfully updated." }
+      format.json { render :show, status: :ok, location: @article }
+    else
+      format.html { render :edit, status: :unprocessable_entity }
+      format.json { render json: @article.errors, status: :unprocessable_entity }
+    end
+  end
+end
 
+private
+# Use callbacks to share common setup or constraints between actions.
+def set_article
+  @article = Article.find(params[:id])
+end
 
-
+# Only allow a list of trusted parameters through.
+def article_params
+  params.require(:article).permit(:country, :name, :city, :street, :extra)
+end
 
 end
